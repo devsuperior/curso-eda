749bfb16b11665e7f8ec56cd98203bb6a7b18671
Alexandre Oliveira <alexandreoliveira@MacBookAir.local>
2023-10-13 12:07:59 -0300

    Busca sequencial ordenada em java, c#, js e python

---
+++ csharp/busca_sequencial_ordenada.cs
@@ -0,0 +1,33 @@
+using System;
+
+/**
+ *  n = number of elements array
+ *
+ *  time: O(n)
+ *  space: O(n)
+ */
+
+public class busca_sequencial_ordenada
+{
+    public static int buscaSequencialOrdenada(int[] nums, int key) {
+        Array.Sort(nums);
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == key)
+                return i;
+        }
+        return -1;
+    }
+    
+    public static void Main(string[] args)
+    {
+        int[] nums = {28, 6, -2, 9, 16, 20, 23, 8};
+        int key = 20;
+        int result = buscaSequencialOrdenada(nums, key);
+        
+        if (result > -1) {
+            Console.WriteLine("Elemento encontrado na posicao " + result);
+        } else {
+            Console.WriteLine("Elemento " + key + " não encontrado no array");
+        }
+    }
+}
---
+++ java/busca_sequencial_ordenada.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class busca_sequencial_ordenada {
+
+    /*
+     *  n = number of elements array
+     * 
+     *  time: O(n)
+     *  space: O(n)
+     */
+
+    public static int buscaSequencialOrdenada(int[] nums, int key) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == key)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {28, 6, -2, 9, 16, 20, 23, 8};
+        int key = 20;
+        int result = buscaSequencialOrdenada(nums, key);
+        
+        if (result > -1) {
+            System.out.println("Elemento encontrado na posicao " + result);
+        } else {
+            System.out.println("Elemento " + key + " não encontrado no array");
+        }
+
+        int nums1[] = {4, 6, -3, 21, 55, 91, 2};
+        int key1 = 11;
+        result = buscaSequencialOrdenada(nums1, key1);
+
+        if (result > -1) {
+            System.out.println("Elemento encontrado na posicao " + result);
+        } else {
+            System.out.println("Elemento " + key1 + " não encontrado no array");
+        }
+    }
+}

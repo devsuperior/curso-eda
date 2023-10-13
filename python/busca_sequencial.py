3f8182c422f963d511739a17117a90d214c5862f
Alexandre Oliveira <alexandreoliveira@MacBookAir.local>
2023-10-13 11:22:42 -0300

    Busca sequencial em python

---
+++ python/busca_sequencial.py
@@ -0,0 +1,36 @@
+from typing import List
+
+#   n = number of elements array
+#
+#   time: O(n)
+#   space: O(n)
+#
+
+def busca_sequencial(nums: List[int], key: int) -> int:
+    for i, num in enumerate(nums):
+        if num == key:
+            return i
+    return -1
+    
+nums = [28, 6, -2, 9, 16, 20, 23, 8]
+key = 20
+
+result = busca_sequencial(nums, key)
+
+if result != -1:
+    print(f"Elemento encontrado na posicao {result}")
+else:
+    print(f"Elemento {key} não encontrado no array")
+
+nums = [4, 6, -3, 21, 55, 91, 2]
+key = 11
+
+result = busca_sequencial(nums, key)
+
+if result != -1:
+    print(f"Elemento encontrado na posicao {result}")
+else:
+    print(f"Elemento {key} não encontrado no array")
+
+
+        
